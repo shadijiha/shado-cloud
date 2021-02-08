603b3d39e18b2fde78471f234a1abd43265a0a89
Shadi Jiha <shadijiha@gmail.com>
2021-02-08 08:51:22 -0500

    Update email.blade.php

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,59 @@
 @extends('layouts.app')
 
+@section('body_class') login_body @endsection
+
+@section('scripts')
+    <!-- Background changing script -->
+    <script>
+        @php
+            // Get all backgrounds with php
+            $files = \Illuminate\Support\Facades\File::allFiles(public_path("/images/backgrounds/"));
+            $file_src = array();
+
+            foreach($files as $file)
+            {
+                array_push($file_src, url("/images/backgrounds/") . "/" . $file->getFilename());
+            }
+        @endphp
+
+        const change_bg_every = 20; // Seconds
+        setInterval(randomBg, change_bg_every * 1000);
+
+        function randomBg() {
+            const backgrounds = {!! json_encode($file_src) !!};
+
+            let bg_name = backgrounds[Math.floor(Math.random() * backgrounds.length)].replaceAll("\\", "\\\\");
+            document.body.style.backgroundImage = `url('${bg_name}')`;
+        }
+
+        window.addEventListener("load", randomBg);
+    </script>
+@endsection
+
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+    <div class="wrapper">
+        <h1>Reset Password</h1>
+        @if($errors->any())
+            {!! implode('', $errors->all('<div class="error_message">:message</div>')) !!}
+        @endif
+        <form method="POST" action="{{ route('password.email') }}">
+            @csrf
+            <input type="email" placeholder="Email" id="email" name="email"
+                   class="@error('email') is_invalid @enderror" value="{{ old('email') }}" required
+                   autocomplete="email"/>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
+            <input type="submit" value="SEND PASSWORD RESET LINK"/>
+        </form>
+        <div class="bottom-text">
+        </div>
+        <div class="socials">
+            <a href="#"><i class="fab fa-facebook-f"></i></a>
+            <a href="#"><i class="fab fa-twitter"></i></a>
+            <a href="#"><i class="fab fa-pinterest"></i></a>
+            <a href="#"><i class="fab fa-linkedin-in"></i></a>
         </div>
     </div>
-</div>
+    <div id="overlay-area">
+
+    </div>
 @endsection
