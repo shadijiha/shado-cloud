5b6d134ff2ad24aea3fec3b683d2dc6046d15354
Shadi Jiha <shadosite@gmail.com>
2022-01-15 18:19:55 -0500

    Added backendhost base url to swagger

--- src/main.ts
+++ src/main.ts
@@ -19,6 +19,7 @@ async function bootstrap() {
 		.setDescription("The Shado Cloud API description")
 		.setVersion("1.0")
 		.addTag("")
+		.setBasePath(process.env.BACKEND_HOST ?? "")
 		.build();
 	const document = SwaggerModule.createDocument(app, config);
 	SwaggerModule.setup("api", app, document);
