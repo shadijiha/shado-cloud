726efeb82a2556c10dcfe861f494618b886770e7
Shadi Jiha <shadosite@gmail.com>
2022-03-20 20:17:25 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,8 @@
-date=$(date'+%Y-%m-%d%H-%M-%S')
-
 pm2 stop "shado-cloud-backend"
 git pull
 npm install
 npm run build
-npm run typeorm:generate -- -n $date.
-npm run typeorm:migrate
+npm run typeorm:generate -- -n "hehexd".
 npm run build
+npm run typeorm:migrate
 pm2 restart "shado-cloud-backend"
